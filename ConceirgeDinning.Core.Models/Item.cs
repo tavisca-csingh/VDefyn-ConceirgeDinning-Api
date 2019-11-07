89b5c3b789b4ed08e3511625d5622355ae6cc0cb
tavisca-mswarnesh <51902860+tavisca-mswarnesh@users.noreply.github.com>
2019-11-07 10:11:37 +0530

    Added food order payment Api

---
+++ ConceirgeDinning.Core.Models/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace ConceirgeDinning.Contracts.Models
+{
+    
+    public class Item
+    {
+        public string ItemName { get; set; }
+        public int quantity { get; set; }
+        public int points { get; set; }
+            
+    }
+    
+}
---
+++ ConceirgeDinning.Core.ServicesImplementation/FoodOrdering/PaymentInitiator.cs
@@ -0,0 +1,48 @@
+﻿using ConceirgeDinning.Contracts.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ConceirgeDiningDAL.Models;
+
+namespace ConceirgeDinning.ServicesImplementation.FoodOrdering
+{
+    public class PaymentInitiator
+    {
+        sql12310325Context conciergeContext = new sql12310325Context();
+        public int AddEntryInOrderTable(OrderResponse orderResponse)
+        {
+            var restaurant = conciergeContext.RestaurantNames.Find(orderResponse.RestaurantId);
+            if (restaurant == null)
+            {
+                //stores restaurent name
+                RestaurantNames restaurantNames = new RestaurantNames();
+                restaurantNames.RestaurantId = orderResponse.RestaurantId;
+                restaurantNames.RestaurantName = orderResponse.RestaurantName;
+                conciergeContext.RestaurantNames.Add(restaurantNames);
+                conciergeContext.SaveChanges();
+            }
+            Ordering ordering = new Ordering();
+            ordering.UserId = orderResponse.UserId;
+            ordering.RestaurantId = orderResponse.RestaurantId;
+            ordering.Status = "Order Placed";
+            ordering.TotalPoints = orderResponse.TotalPoints;
+            conciergeContext.Ordering.Add(ordering);
+            conciergeContext.SaveChanges();
+            return ordering.OrderId;
+        }
+        public void AddEntriesInOrderDetailTable(int orderId,List<Item> items)
+        {
+            
+            foreach (var item in items)
+            {
+                OrderDetails orderDetails = new OrderDetails();
+                orderDetails.OrderId = orderId;
+                orderDetails.ItemName = item.ItemName;
+                orderDetails.Price = item.points;
+                orderDetails.Quantity = item.quantity;
+                conciergeContext.OrderDetails.Add(orderDetails);
+            }
+            conciergeContext.SaveChanges();
+        }
+    }
+}
