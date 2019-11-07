3b522f95b2383087bd00f5391de8024468eb513a
tavisca-mswarnesh <51902860+tavisca-mswarnesh@users.noreply.github.com>
2019-11-07 10:03:11 +0530

    Added Database models for food ordering

---
+++ ConceirgeDiningDAL/Models/OrderDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConceirgeDiningDAL.Models
+{
+    public partial class OrderDetails
+    {
+        public int OrderId { get; set; }
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+        public virtual Ordering Order { get; set; }
+    }
+}
