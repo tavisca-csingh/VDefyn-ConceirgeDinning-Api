10146eb572b60fc3ec27ea9c39bc2d1ef7937089
tavisca-mswarnesh <51902860+tavisca-mswarnesh@users.noreply.github.com>
2019-11-10 21:48:51 +0530

    Added test cases for food ordering

---
+++ ConceirgeDining.ServiceImplementation.Tests/FoodOrderingTests/MenuItemListTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ConceirgeDinning.ServicesImplementation.FoodOrdering;
+using ConceirgeDinning.Contracts.Models;
+
+namespace ConceirgeDining.ServiceImplementation.Tests.FoodOrderingTests
+{
+    public class MenuItemListTests
+    {
+        MenuItemList menuItemList = new MenuItemList(); 
+        [Fact]
+        public void Get_Restaurant_Menu_Details_By_RestaurantId_For_Valid_Id()
+        {
+            var itemList = menuItemList.GetMenus("1234","Zomato");
+            bool actual = itemList is List<Category>;
+            Assert.True(actual);
+        }
+        [Fact]
+        public void Get_Restaurant_Menu_Details_By_RestaurantId_For_Invalid_Id()
+        {
+            var itemList = menuItemList.GetMenus("1234", "UsRestaurant");
+            bool actual = itemList is null;
+            Assert.True(actual);
+        }
+    }
+}
