43967d353352e5268e3bdbebbb4315d6a665864c
tavisca-mswarnesh <51902860+tavisca-mswarnesh@users.noreply.github.com>
2019-11-12 11:54:16 +0530

    Added Data Access Layer for Loggers

---
+++ ConceirgeDining.LoggerDAL/Models/LogContext.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConceirgeDining.LoggerDAL.Models
+{
+    public class LogContext
+    {
+        public void ConnectTOMongoDB()
+        {
+            
+            var connectionString = "mongodb+srv://mattapalliswarnesh:lthliCuE4xi80DOE@logs-et0xz.mongodb.net/ConceirgeLogs?retryWrites=true&w=majority";
+            var client = new MongoClient(connectionString);
+            var dataBase = client.GetDatabase("ConceirgeLogs");
+            var collection = dataBase.GetCollection<LogInfo>("Testing");
+            LogInfo logInfo = new LogInfo();
+            logInfo.SessionId =  Guid.NewGuid().ToString();
+            logInfo.ServiceId = "5678";
+            logInfo.UserId = "Anonomus";
+            logInfo.TimeStamp = DateTime.Now;
+            logInfo.RequestFromUser = "abc";
+            logInfo.RequestToSupplier = "def";
+            logInfo.ResponseFromSupplier = "ghi";
+            logInfo.ResponseToUser = "jkl";
+            logInfo.ResponseTime = new TimeSpan(1000);
+            collection.InsertOne(logInfo);
+        }
+    }
+}
---
+++ ConceirgeDining.LoggerDAL/Models/LogInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ConceirgeDining.LoggerDAL.Models
+{
+    public class LogInfo
+    {
+        [BsonElement("SessionId")]
+        public string  SessionId { get; set; }
+        [BsonElement("ServiceId")]
+        public string ServiceId { get; set; }
+        [BsonElement("UserId")]
+        public string UserId { get; set; }
+        [BsonElement("TimeStamp")]
+        [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
+        public DateTime TimeStamp { get; set; }
+        [BsonElement("RequestFromUser")]
+        public string RequestFromUser { get; set; }
+        [BsonElement("RequestToSupplier")]
+        public string RequestToSupplier { get; set; }
+        [BsonElement("ResponseFromSupplier")]
+        public string ResponseFromSupplier { get; set; }
+        [BsonElement("ResponseToUser")]
+        public string ResponseToUser { get; set; }
+        [BsonElement("ResponseTime")]
+        public TimeSpan ResponseTime { get; set; }
+    }
+}
